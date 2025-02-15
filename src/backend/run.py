3633db17b8c012e4cc59bc61406db7e76481d43a
Alina Salman <114557579+alinasalman@users.noreply.github.com>
2025-02-15 01:43:37 -0800

    Revert "Notification with text-to-speech with ElevenLabs"

--- src/backend/run.py
+++
@@ -1,9 +0,0 @@
-# run.py
-from app import create_app
-
-# Create an instance of the Flask application using our factory function.
-app = create_app()
-
-if __name__ == '__main__':
-    # Start the Flask development server
-    app.run(debug=True)
